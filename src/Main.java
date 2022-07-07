e73f638cd919d64ea456fb4f7d6addff2f291d91
patress99 <49107052+patress99@users.noreply.github.com>
2022-07-07 17:08:31 +0200

    Adding for into main.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("New Application Created");
+
+
+        for (int i = 0; i < 9; ++i) {
+            System.out.println(i);
+        }
     }
 }
